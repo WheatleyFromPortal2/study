7b363f9092b877f7d8b748b518a388ce6137496a
Hunter Dorrill <hunterdorrill@icloud.com>
2024-05-09 09:53:06 -0500

    reduce batch size

--- aicomp.py
+++ aicomp.py
@@ -1,8 +1,9 @@
+batchSize = 1
 print("Importing AI, this might take a second")
 from semantic_text_similarity.models import WebBertSimilarity
 #from semantic_text_similarity.models import ClinicalBertSimilarity
 
-web_model = WebBertSimilarity(device='cpu', batch_size=10) #defaults to GPU prediction
+web_model = WebBertSimilarity(device='cpu', batch_size=batchSize) #defaults to GPU prediction
 
 #clinical_model = ClinicalBertSimilarity(device='cpu', batch_size=10) #defaults to GPU prediction
 def AiCompare(answer, inputed):
